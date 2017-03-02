043d739b9257acb3e3328510be383af83dbaf397
Sawan Choudhary <sawan.choudhary@calsoftinc.com>
2017-03-02 15:26:06 +0530

    VM Creation Portal

---
+++ vmcreationportal/WebPortal/VM_files/CSV/demo_ssh.py
@@ -0,0 +1,43 @@
+__author__ = "Sawan Choudhary"
+__copyright__ = "Copyright 2017, CalsoftInc"
+__maintainer__ = "Calsoft IT"
+
+#------------------------------------------------------------------------------------------------------------
+
+from pexpect import pxssh
+import getpass
+from send_error_mail import send_error_mail_fn
+
+
+print "Starting demo SSH"
+f = open("/home/eng/vmcreationportal/WebPortal/VM_files/CSV/hosts1")
+file_name = f.readline().strip()
+file_name1 = file_name.split(" ")
+f.close()
+#print file_name1[0]
+try:                                                            
+    s = pxssh.pxssh()
+    #hostname = raw_input('hostname: ')
+    hostname = file_name1[0]
+    username = 'ituser'
+    password = 'Calsoft@123'
+    #password = getpass.getpass('password: ')
+    s.login (hostname, username, password)
+    s.sendline ('uptime')   # run a command
+    s.prompt()             # match the prompt
+    #print s.before          # print everything before the prompt.
+    s.sendline ('ls -l')
+    s.prompt()
+    #print s.before
+    s.sendline ('df')
+    s.prompt()
+    #print s.before
+    s.logout()
+    print "Finished demo SSH"
+except pxssh.ExceptionPxssh, e:
+    print "pxssh failed on login."
+    print str(e)
+    error_ssh = "pxssh failed to login" + " " + str(e)
+    send_error_mail_fn(error_ssh)
+    raise SystemError("Error may be due already existing key of same IP")
+  
---
+++ vmcreationportal/WebPortal/VM_files/CSV/vm_creation_portal_start.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+#
+
+# author = "Sawan Choudhary"
+# copyright = "Copyright 2017, CalsoftInc"
+# maintainer = "Calsoft IT"
+
+python /home/eng/vmcreationportal/WebPortal/VM_files/CSV/create_dict_from_csv.py
+
+python /home/eng/vmcreationportal/WebPortal/VM_files/CSV/createvm.py -s 172.17.75.51 -u root -p Calsoft@123 -v /home/eng/vmcreationportal/WebPortal/VM_files/CSV/VM_Creation_Portal_Template-disk1.vmdk -f /home/eng/vmcreationportal/WebPortal/VM_files/CSV/VM_Creation_Portal_Template.ovf
+
+if [ $? = 0 ]; then
+
+    ansible-playbook -i /home/eng/vmcreationportal/WebPortal/VM_files/CSV/hosts /home/eng/vmcreationportal/WebPortal/VM_files/CSV/main_change_vm_config.yml
+
+    ansible-playbook -i /home/eng/vmcreationportal/WebPortal/VM_files/CSV/hosts /home/eng/vmcreationportal/WebPortal/VM_files/CSV/main_power_on_vm.yml
+
+    python /home/eng/vmcreationportal/WebPortal/VM_files/CSV/parse_new_vm_ip1.py -s 172.17.75.51 -u root -p Calsoft@123
+
+    python /home/eng/vmcreationportal/WebPortal/VM_files/CSV/demo_ssh.py
+
+    if [ $? = 0 ]; then
+
+        ansible-playbook -i /home/eng/vmcreationportal/WebPortal/VM_files/CSV/hosts1 /home/eng/vmcreationportal/WebPortal/VM_files/CSV/main_install_pck.yml
+
+        python /home/eng/vmcreationportal/WebPortal/VM_files/CSV/send_process_complete_mail.py
+
+        python /home/eng/vmcreationportal/WebPortal/VM_files/CSV/finish_task.py
+
+    else
+        echo "Failure,to SSH to new VM"
+    fi
+
+
+else
+    echo "Failure, could not create VM"
+fi
+
+
